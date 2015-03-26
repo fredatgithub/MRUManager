166fe51b0eebcad4e9eeea85ce2b3f3895946a83
Freddy Juhel <domaine75@gmail.com>
2015-03-26 14:22:17 +0100

    initial commit

---
+++ MRUManager_dotNet4_example/FormMain.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+
+namespace MRUManager_dotNet4_example
+{
+  public partial class FormMain : Form
+  {
+    private MRUManager mruManager;
+
+    public FormMain()
+    {
+      InitializeComponent();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      mruManager = new MRUManager(
+        //the menu item that will contain the recent files
+        recentFilesToolStripMenuItem,
+
+        //the name of your program
+        "myProgram",
+
+        //the funtion that will be called when a recent file gets clicked.
+        myOwnRecentFileGotClicked_handler,
+
+        //an optional function to call when the user clears the list of recent items
+        myOwnRecentFilesGotCleared_handler);
+    }
+
+    private void myOwnRecentFileGotClicked_handler(object obj, EventArgs evt)
+    {
+      string fName = (obj as ToolStripItem).Text;
+      if (!File.Exists(fName))
+      {
+        if (MessageBox.Show(string.Format("{0} doesn't exist. Remove from recent workspaces?", fName), "File not found", MessageBoxButtons.YesNo) == DialogResult.Yes)
+        {
+          mruManager.RemoveRecentFile(fName);
+        }
+
+        return;
+      }
+
+      //do something with the file here
+      MessageBox.Show(string.Format("Through the 'Recent Files' menu item, you opened: {0}", fName));
+    }
+
+    private void myOwnRecentFilesGotCleared_handler(object obj, EventArgs evt)
+    {
+      //prior to this function getting called, all recent files in the registry and 
+      //in the program's 'Recent Files' menu are cleared.
+
+      //perhaps you want to do something here after all this happens
+      MessageBox.Show("You just cleared all recent files.");
+    }
+
+    private void openToolStripMenuItem_Click(object obj, EventArgs evt)
+    {
+      FileDialog openFileDlg = new OpenFileDialog();
+      openFileDlg.InitialDirectory = Environment.CurrentDirectory;
+      if (openFileDlg.ShowDialog() != DialogResult.OK)
+      {
+        return;
+      }
+
+      string openedFile = openFileDlg.FileName;
+
+      //Now give it to the MRUManager
+      mruManager.AddRecentFile(openedFile);
+
+      //do something with the file here
+      MessageBox.Show("Through the 'Open' menu item, you opened: " + openedFile);
+    }
+  }
+}
---
+++ MRUManager_dotNet4_example/MRUManager.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace MRUManager_dotNet4_example
+{
+  public class MRUManager
+  {
+    #region Private members
+    private string NameOfProgram;
+    private string SubKeyName;
+    private ToolStripMenuItem ParentMenuItem;
+    private Action<object, EventArgs> OnRecentFileClick;
+    private Action<object, EventArgs> OnClearRecentFilesClick;
+
+    private void _onClearRecentFiles_Click(object obj, EventArgs evt)
+    {
+      try
+      {
+        RegistryKey rK = Registry.CurrentUser.OpenSubKey(SubKeyName, true);
+        if (rK == null)
+        {
+          return;
+        }
+
+        string[] values = rK.GetValueNames();
+        foreach (string valueName in values)
+        {
+          rK.DeleteValue(valueName, true);
+        }
+
+        rK.Close();
+        ParentMenuItem.DropDownItems.Clear();
+        ParentMenuItem.Enabled = false;
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+
+      if (OnClearRecentFilesClick != null)
+      {
+        OnClearRecentFilesClick(obj, evt);
+      }
+    }
+
+    private void _refreshRecentFilesMenu()
+    {
+      RegistryKey rK;
+      string s;
+      ToolStripItem tSI;
+
+      try
+      {
+        rK = Registry.CurrentUser.OpenSubKey(SubKeyName, false);
+        if (rK == null)
+        {
+          ParentMenuItem.Enabled = false;
+          return;
+        }
+      }
+      catch (Exception ex)
+      {
+        MessageBox.Show("Cannot open recent files registry key:\n" + ex);
+        return;
+      }
+
+      ParentMenuItem.DropDownItems.Clear();
+      string[] valueNames = rK.GetValueNames();
+      foreach (string valueName in valueNames)
+      {
+        s = rK.GetValue(valueName, null) as string;
+        if (s == null)
+        {
+          continue;
+        }
+
+        tSI = ParentMenuItem.DropDownItems.Add(s);
+        tSI.Click += new EventHandler(OnRecentFileClick);
+      }
+
+      if (ParentMenuItem.DropDownItems.Count == 0)
+      {
+        ParentMenuItem.Enabled = false;
+        return;
+      }
+
+      ParentMenuItem.DropDownItems.Add("-");
+      tSI = ParentMenuItem.DropDownItems.Add("Clear list");
+      tSI.Click += new EventHandler(_onClearRecentFiles_Click);
+      ParentMenuItem.Enabled = true;
+    }
+    #endregion
+
+    #region Public members
+    public void AddRecentFile(string fileNameWithFullPath)
+    {
+      string s;
+      try
+      {
+        // Registry location: HKCU\Software\Application name\MRU\
+        RegistryKey rK = Registry.CurrentUser.CreateSubKey(SubKeyName, RegistryKeyPermissionCheck.ReadWriteSubTree);
+        for (int i = 0; true; i++)
+        {
+          s = rK.GetValue(i.ToString(), null) as string;
+          if (s == null)
+          {
+            rK.SetValue(i.ToString(), fileNameWithFullPath);
+            rK.Close();
+            break;
+          }
+          else if (s == fileNameWithFullPath)
+          {
+            rK.Close();
+            break;
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+
+      _refreshRecentFilesMenu();
+    }
+
+    public void RemoveRecentFile(string fileNameWithFullPath)
+    {
+      try
+      {
+        RegistryKey rK = Registry.CurrentUser.OpenSubKey(SubKeyName, true);
+        string[] valuesNames = rK.GetValueNames();
+        foreach (string valueName in valuesNames)
+        {
+          if ((rK.GetValue(valueName, null) as string) == fileNameWithFullPath)
+          {
+            rK.DeleteValue(valueName, true);
+            _refreshRecentFilesMenu();
+            break;
+          }
+        }
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+
+      _refreshRecentFilesMenu();
+    }
+    #endregion
+
+    /// <exception cref="ArgumentException">
+    /// If anything is null or nameOfProgram contains a forward slash or is empty.
+    /// </exception>
+    public MRUManager(ToolStripMenuItem parentMenuItem, string nameOfProgram, Action<object, EventArgs> onRecentFileClick, Action<object, EventArgs> onClearRecentFilesClick = null)
+    {
+      if (parentMenuItem == null || onRecentFileClick == null ||
+        nameOfProgram == null || nameOfProgram.Length == 0 || nameOfProgram.Contains("\\"))
+      {
+        MessageBox.Show("Bad argument.");
+        // throw new ArgumentException("Bad argument.");
+      }
+
+      ParentMenuItem = parentMenuItem;
+      NameOfProgram = nameOfProgram;
+      OnRecentFileClick = onRecentFileClick;
+      OnClearRecentFilesClick = onClearRecentFilesClick;
+      SubKeyName = string.Format("Software\\{0}\\MRU", NameOfProgram);
+      _refreshRecentFilesMenu();
+    }
+  }
+}
